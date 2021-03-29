c95e93caf4da3cb3bfee53ce6331d3888ae82c32
Danendraa.djlantik <65715562+Danendra-Mrx@users.noreply.github.com>
2021-03-29 20:28:22 +0700

    Update destroy.py

--- destroy.py
+++ destroy.py
@@ -8,17 +8,15 @@ def usage():
     print "#----------------------------------------------------------#"
     print "#   \033[1;91mCommand: " "python2 destroy.py " "<ip> <port> <packet> \033[1;32m #"
     print "#                                                        ##"
-    print "#\033[1;91mCreator:Danendra \033[1;32m##      #      #                     ##"
-    print "#\033[1;91mTeam   : HAC       \033[1;32m##     #      #                     ##"
+    print "#\033[1;91mCreator:Danendra Djlantik\033[1;32m##      #      #                     ##"
     print "#\033[1;91mVersion:1.0        \033[1;32m##      #      #                     ##"
-    print "#\033[1;91mTQAdmin:  Danendra   ##"
-    print "#\033[1;91m       :TAN91212  IAOER59334 UIO1231 MKO51385##"
+    print "#\033[1;91mCredit:  Danendra Djlantik   ##"
     print "#                     \033[1;91m ##     \033[1;32m#  \033[1;91m  \033[1;32   ##"
     print "#                     \033[1;91m##  \033[1;32m###   \033[1;91m  \033[1;32m   ##"
     print "#               \033[1;91m<--[HOME ALONE CHILD]-->         \033[1;32m  ##"
     print "############################################################"
-    print "    This Is The Final Tools Of DDOS-POLRI & Others"
-    print "    Maked With Love By Danendra"
+    print "    ~This Is The Final Tools Of DDOS-POLRI & Others!"
+    print "    !Maked With Love By Danendra"
     
    
 def flood(victim, vport, duration):
